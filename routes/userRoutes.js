0dcb5d22540112a419eb4b2060c6bc2a06d9a4f6
Alberto Codoñer Gea <alberto.cg2197@gmail.com>
2025-03-04 19:34:54 +0100

    feat: Edit user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,8 @@ const {Router} = require('express');
 
 const {registerUser, loginUser,  getUser, changeAvatar, editUser, getAuthors} = require("../controllers/userControllers")
 
+const authMiddleware = require('../middleware/authMiddleware')
+
 const router = Router();
 
 
@@ -10,8 +12,8 @@ router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/:id', getUser)
 router.get('/', getAuthors)
-router.post('/change-avatar', changeAvatar)
-router.patch('/edit-user', editUser)
+router.post('/change-avatar',authMiddleware,  changeAvatar)
+router.patch('/edit-user', authMiddleware,  editUser)
 
 
 
